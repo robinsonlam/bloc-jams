5c859df8e817da6d3ace1b83213ac9c1ac18d211
Robinson Lam <robinsonlam94@gmail.com>
2014-07-12 12:34:36 +1000

    Converted landing page to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,39 @@
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+ //require('./landing');
+ //require('./album');
+ //require('./collection');
+ //require('./profile');
+
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope',
+     function($scope) {
+
+         $scope.titleText = "Bloc Jams, ver. Rob - Click here to shuffle album";
+
+         $scope.subText = "Turn the muuuusic up!";
+
+         $scope.albumURLs = [
+             '/images/album-placeholders/album-1.jpg',
+             '/images/album-placeholders/album-2.jpg',
+             '/images/album-placeholders/album-3.jpg',
+             '/images/album-placeholders/album-4.jpg',
+             '/images/album-placeholders/album-5.jpg',
+             '/images/album-placeholders/album-6.jpg',
+             '/images/album-placeholders/album-7.jpg',
+             '/images/album-placeholders/album-8.jpg',
+             '/images/album-placeholders/album-9.jpg',
+         ];
+
+         $scope.albumShuffle = function() {
+             function shuffle(o) { //v1.0
+                 for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+                 return o;
+             };
+
+             shuffle($scope.albumURLs);
+         };
+
+         $scope.subTextClicked = function() {
+             $scope.subText += '!';
+         };
+
+     }
+ ]);
