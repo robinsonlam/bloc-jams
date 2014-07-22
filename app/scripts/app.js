1d4143cea3f05c350574a53ec8eb8777fb925093
Phil Spitler <prs@sproutkey.com>
2014-07-22 04:25:49 +0000

    finished volume-control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -339,6 +339,13 @@
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer',
      function($scope, SongPlayer) {
          $scope.songPlayer = SongPlayer;
+         $scope.volumeClass = function() {
+             return {
+               'fa-volume-off': SongPlayer.volume == 0,
+               'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+               'fa-volume-up': SongPlayer.volume > 70
+             }
+           }
          SongPlayer.onTimeUpdate(function(event, time){
                 $scope.$apply(function(){
                 $scope.playTime = time;
@@ -360,6 +367,7 @@
          currentSong: null,
          currentAlbum: null,
          playing: false,
+         volume: 90,
 
          play: function() {
              this.playing = true;
@@ -379,6 +387,12 @@
      onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+        setVolume: function(volume) {
+          if(currentSoundFile){
+            currentSoundFile.setVolume(volume);
+          }
+          this.volume = volume;
+        },
          setSong: function(album, song) {
 
              if (currentSoundFile) {
@@ -390,6 +404,8 @@
                  formats: ["mp3"],
                  preload: true
              });
+
+            currentSoundFile.setVolume(this.volume);
             
             currentSoundFile.bind('timeupdate', function(e){
                 $rootScope.$broadcast('sound:timeupdate', this.getTime());
